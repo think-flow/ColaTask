3bc07b9ba563b504c0e33825f4f9e19636687550
Cole <564603061@qq.com>
2025-04-19 16:11:00 +0800

    d

---
+++ src/ColaTask/CoTaskScheduler.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Concurrent;
+
+namespace ColaTask;
+
+public class CoTaskScheduler : TaskScheduler
+{
+    private static readonly Lazy<CoTaskScheduler> _instance = new Lazy<CoTaskScheduler>(() => new CoTaskScheduler(), true);
+
+    //用于存放任务的容器
+    private readonly BlockingCollection<Task> _tasks = new BlockingCollection<Task>();
+
+    //用于执行任务的线程
+    private readonly Thread _worker;
+
+    private CoTaskScheduler()
+    {
+        _worker = new Thread(Work)
+        {
+            IsBackground = true,
+            Name = "CoTaskScheduler Executor"
+        };
+        _worker.UnsafeStart();
+    }
+
+    public static CoTaskScheduler Instance => _instance.Value;
+
+    private void Work()
+    {
+        foreach (var task in _tasks.GetConsumingEnumerable())
+        {
+            TryExecuteTask(task);
+        }
+    }
+
+    protected override IEnumerable<Task> GetScheduledTasks() => _tasks.ToArray();
+
+    protected override void QueueTask(Task task) => _tasks.Add(task);
+
+    protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+    {
+        // // 在单线程Scheduler中，要确保当前线程是本Scheduler中的线程，才执行任务
+        // bool isCurrentThread = Thread.CurrentThread == _worker;
+        // if (!isCurrentThread)
+        // {
+        //     //表示，当前Scheduler不处理该任务，请将任务加入队列中
+        //     return false;
+        //     // throw new InvalidOperationException($"尝试在错误线程({Thread.CurrentThread.ManagedThreadId})上执行任务，应该在线程{_worker.ManagedThreadId}上执行");
+        // }
+        //
+        // //内联执行任务
+        // return TryExecuteTask(task);
+        return Thread.CurrentThread == _worker && TryExecuteTask(task);
+    }
+}
---
+++ test/ColaTask.Test/CoTaskSchedulerTest.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+using Xunit.Abstractions;
+
+namespace ColaTask.Test;
+
+public class CoTaskSchedulerTest(ITestOutputHelper output)
+{
+    [Fact]
+    public void Should_Same_Instance()
+    {
+        var list = new ConcurrentBag<CoTaskScheduler>();
+        var temp = CoTaskScheduler.Instance;
+        Parallel.For(0, 20, _ =>
+        {
+            var instance = CoTaskScheduler.Instance;
+            list.Add(instance);
+        });
+        //确保只有一个CoTaskScheduler实例
+        Assert.All(list, instance => Assert.Same(temp, instance));
+    }
+
+    [Fact]
+    public async Task Should_Single_Thread()
+    {
+        int mainThreadId = Thread.CurrentThread.ManagedThreadId;
+        output.WriteLine($"main thread id {mainThreadId}");
+
+        //获取单线程调度器
+        var scheduler = CoTaskScheduler.Instance;
+        //在单线程调度器上执行任务
+        await Task.Factory.StartNew(async () =>
+        {
+            int taskThreadId = Thread.CurrentThread.ManagedThreadId;
+            output.WriteLine($"task thread id {taskThreadId}");
+
+            try
+            {
+                using var httpClient = new HttpClient();
+                var response = httpClient.GetAsync("https://www.baidu.com");
+                var timeout = Task.Delay(200);
+                await Task.WhenAny(timeout, response);
+                Assert.Equal(taskThreadId, Thread.CurrentThread.ManagedThreadId);
+
+                if (response.IsCompleted)
+                {
+                    string _ = await response.Result.Content.ReadAsStringAsync();
+                    Assert.Equal(taskThreadId, Thread.CurrentThread.ManagedThreadId);
+                }
+            }
+            catch
+            {
+                // ignored
+            }
+
+            await Task.Delay(50);
+            Assert.Equal(taskThreadId, Thread.CurrentThread.ManagedThreadId);
+
+            await Task.Delay(100);
+            Assert.Equal(taskThreadId, Thread.CurrentThread.ManagedThreadId);
+
+            output.WriteLine("task thread completed");
+        }, CancellationToken.None, TaskCreationOptions.None, scheduler).Unwrap();
+
+        output.WriteLine("main thread completed");
+    }
+
+    [Fact]
+    public async Task Should_Async()
+    {
+        //获取单线程调度器
+        var scheduler = CoTaskScheduler.Instance;
+        var sw = new Stopwatch();
+        await Task.Factory.StartNew(async () =>
+        {
+            sw.Start();
+            var task1 = Task.Delay(100);
+            var task2 = Task.Delay(150);
+            var task3 = Task.Delay(300);
+            await Task.WhenAll(task1, task2, task3);
+            sw.Stop();
+        }, CancellationToken.None, TaskCreationOptions.None, scheduler).Unwrap();
+
+        //如果是异步的，总花费时间应该为较长的300毫秒，
+        //如果是同步的，总花费时间应该为(100+150+300)毫秒
+        long ms = sw.ElapsedMilliseconds;
+        output.WriteLine($"it takes {ms} milliseconds");
+        //允许误差30毫秒
+        Assert.True(ms - 300 < 30);
+    }
+
+    [Fact]
+    public async Task Should_Sync()
+    {
+        //获取单线程调度器
+        var scheduler = CoTaskScheduler.Instance;
+        var sw = new Stopwatch();
+        await Task.Factory.StartNew(async () =>
+        {
+            sw.Start();
+            await Task.Delay(100);
+            await Task.Delay(150);
+            await Task.Delay(300);
+            sw.Stop();
+        }, CancellationToken.None, TaskCreationOptions.None, scheduler).Unwrap();
+
+        //如果是异步的，总花费时间应该为较长的300毫秒上下，
+        //如果是同步的，总花费时间应该为(100+150+300)550毫秒上下
+        long ms = sw.ElapsedMilliseconds;
+        output.WriteLine($"it takes {ms} milliseconds");
+        //允许误差30毫秒
+        Assert.True(ms - (100 + 150 + 300) < 30);
+    }
+
+    //测试死锁问题
+    [Fact]
+    public async Task Should_Dead_lock()
+    {
+        //获取单线程调度器
+        var scheduler = CoTaskScheduler.Instance;
+        await Task.Factory.StartNew(async () =>
+        {
+            using var httpClient = new HttpClient();
+            var content = httpClient.GetStringAsync("https://www.baidu.com").Result;
+            output.WriteLine(content);
+        }, CancellationToken.None, TaskCreationOptions.None, scheduler).Unwrap();
+    }
+}
